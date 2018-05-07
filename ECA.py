4991c877faa79af2f9ca58c0873481e780047292
Fabi <fabperaltar@gmail.com>
2018-05-07 09:14:20 -0500

    Python3 version

---
+++ ECA.py
@@ -0,0 +1,152 @@
+import sys, pygame, random, matplotlib.pyplot as plt, numpy as np
+from pygame.locals import *
+
+color_cell=(45,45,45)
+color_background=(255,255,255)
+sy=0
+
+class celAut:
+
+	rule=[]
+	cells=0
+	gens=0
+	oFreq=0
+	den=0
+
+	def __init__(self, rule, cells, gens):
+		self.rule=rule
+		self.cells=cells
+		self.gens=gens
+		self.t0=[]
+		self.t1=[]
+
+	def setRandomFirstGen(self, dens):
+		d=0
+		for i in range(self.cells):
+			n=random.randint(0, self.cells)
+			x=n%2
+			if x==1:
+				self.t0.append(1)
+				self.oFreq+=1
+				d+=1
+			else:
+				self.t0.append(0)
+				
+		while self.oFreq>=dens:
+			n=random.randint(0, self.cells-1)
+			if self.t0[n]==1:
+				self.t0[n]=0
+				self.oFreq-=1
+			
+		print(self.oFreq)
+
+	def setOneCellFirstGen(self):
+		self.oFreq=1
+		x=self.cells//2
+		for i in range(self.cells):
+			if i==x:
+				self.t0.append(1)
+			else:
+				self.t0.append(0)
+
+	def getNextGen(self):
+		self.oFreq=0
+		for i in range(self.cells):
+			d=(self.t0[((i-1)%self.cells)])*4+(self.t0[i])*2+(self.t0[((i+1)%self.cells)])
+			nc=self.rule[d]
+			if nc:
+				self.oFreq+=1
+			self.t1.append(nc)
+		self.t0=self.t1
+		self.t1=[]
+
+def update_screen(gen):
+	global sy
+	x=0
+	for elem in gen:
+		if elem:
+			screen.fill(color_cell, (x, sy, 1, 1))
+		else:
+			screen.fill(color_background, (x, sy, 1, 1))
+		x+=1
+	sy+=1
+
+def init(rule, w, h):
+	global background, screen
+	pygame.init()
+	screen=pygame.display.set_mode((w, h))
+	pygame.display.set_caption("Regla "+str(rule))
+	screen.fill(color_background)
+
+def getRule(rule):
+	b=[]
+	for i in range(8):
+		b.append(rule%2)
+		rule=rule//2
+	return b
+
+def showAut():
+	while 1:
+		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
+				pygame.quit()
+				sys.exit()
+		pygame.display.flip()
+
+def getAv(freq, gens):
+	av=0
+	for x in range(gens):
+		av+=freq[x]
+	av/=gens
+	return av
+
+def getVar(av, freq, gens):
+	av2=0
+	for x in range(gens):
+		av2+=freq[x]**2
+	av2/=gens
+	var=av2-av**2
+	return var
+
+print("1 Random")
+print("2 Una célula")
+option=int(input())
+print("Introduzca regla")
+rule=int(input())
+rulea=getRule(rule)
+print("Cuantas células?")
+cells=int(input())
+print("Cuantas generaciones?")
+gens=int(input())
+aut=celAut(rulea, cells, gens)
+if option==1:
+	print("Introduzca su densidad")
+	dens=int(input())
+	densC=dens*aut.cells
+	densC=densC//100
+	aut.setRandomFirstGen(densC)
+elif option==2:
+	aut.setOneCellFirstGen()
+else:
+	print("Opción incorrecta")
+
+init(rule, cells, gens)
+avO=[]
+for y in range(aut.gens):
+	update_screen(aut.t0)
+	aut.getNextGen()
+	pygame.display.flip()
+	avO.append(aut.oFreq)
+	
+plt.ion()
+a=np.arange(gens)
+plt.bar(a, height=avO)
+gs=[str(x+1) for x in range(gens)]
+plt.xticks(a, gs)
+av=getAv(avO, gens)
+print("Media=",av)
+var=getVar(av, avO, gens)
+print("Varianza=",var)
+
+showAut()
+
