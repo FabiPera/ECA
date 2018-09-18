a2f4438db03d931319f84406cbab99385cbe24f7
Unknown <fabperaltar@gmail.com>
2018-09-18 12:49:12 -0500

    ECA v2.2.4

--- ECA.h
+++ ECA.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <stdlib.h>
 #include <math.h>
+#include "Configuration.h"
 using namespace std;
 
 #ifndef __ECA__
@@ -10,14 +11,13 @@ using namespace std;
 class ECA{
 	
 	public:
-		int* rule;
-		int* initConfig;
-		int* t0;
-		int* tDam;
+		Configuration rule;
+		Configuration initConfig;
+		Configuration t0;
+		Configuration tDam;
 		int* tFreq;
 		int* dFreq;
 		double* ps;
-		int nCells;
 		int steps;
 		int denPer;
 		int gFreq;
@@ -31,11 +31,7 @@ class ECA{
 
 		/* Setters */
 		void setRule(int rule){
-			this->rule=intToBin(rule, 8);
-		}
-
-		void setCells(int cells){
-			this->nCells=cells;
+			this->rule=Configuration(rule, 8);
 		}
 
 		void setGens(int gens){
@@ -52,130 +48,69 @@ class ECA{
 
 		void setT0(string t0){
 			string str("1");
-			this->t0=new int[this->nCells];
-			this->initConfig=new int[this->nCells];
-			for(int i=0; i<nCells; i++){
-				if((str.compare(t0.substr(i, 1)))==0){
-					this->t0[i]=1;
-					this->initConfig[i]=1;	
-				}
-				else{
-					this->t0[i]=0;
-					this->initConfig[i]=0;
-				}	
-			}
+			this->t0=Configuration(t0.length());
+			this->t0.setConfiguration(t0);
+			this->initConfig=Configuration(t0.length());
+			this->initConfig.setConfiguration(t0);
 		}
 
-		int mod(int a){
-			if(a<0){
-				return (this->nCells)+a;
-			}
-			else{
-				return a%(this->nCells);
-			}	
-		}
-
-		int* intToBin(int n, int size){
-			int* a=new int[size];
-			int* bin=new int[size];
-			int i=0, j=0;
-			while(n){
-				a[i]=n%2;
-				cout << a[i] << endl;
-				n/=2;
-				i++;
-			}
-			i--;
-
-			while(j<size){
-				if(i>=0){
-					bin[i]=a[i];
-					i--;
-				}
-				else{
-					bin[j]=0;
-				}
-				j++;
-			}
-			return bin;
-		}
-
-		int binToInt(int* bin, int size){
-			int n=0;
-			for(int i=0; i<size; ++i){
-				if(bin[i]){
-					n+=pow(2.0, i);
-				}
-			}
-			return n;
-		}
-
-		void getRandomConfiguration(){
-			this->t0=new int[nCells]();
-			this->initConfig=new int[nCells]();
+		void setRandomT0(int l){
+			this->t0=Configuration(l);
+			this->initConfig=Configuration(l);
 			this->gFreq=0;
-			int dens=static_cast<int>((this->denPer)*(this->nCells))/100;
-			int n, x;
-			srand((unsigned) time(0));
-			while((this->gFreq)<dens){
-				n=0+(rand()%static_cast<int>(((this->nCells-1)-0)+1));
-				if(this->t0[n]!=1){
-					this->t0[n]=1;
-					this->initConfig[n]=1;
-					this->gFreq+=1;
-				}
-			}
+			int dens=static_cast<int>((this->denPer)*(t0.length))/100;
+			this->t0.setRandomConfiguration(dens);
+			this->initConfig.setRandomConfiguration(dens);
 		}
 
-		int* evolve(int* t0){
-			int* t1=new int[this->nCells];
+		Configuration evolve(Configuration t0){
+			Configuration t1(t0.length);
 			int n;
-			int neighb[3];
-
-			for(int i=0; i<(this->nCells); i++){
-				neighb[2]=(t0[mod(i-1)]);
-				neighb[1]=(t0[i]);
-				neighb[0]=(t0[mod(i+1)]);
+			Configuration neighb(3);
+			for(int i=0; i<t0.length; i++){
+				neighb.config[2]=t0.getCellValue(i-1);
+				neighb.config[1]=t0.getCellValue(i);
+				neighb.config[0]=t0.getCellValue(i+1);
 				n=0;
-				n=binToInt(neighb, 3);
-				if(this->rule[n]){
-					t1[i]=1;
+				n=neighb.binToInt();
+				if(this->rule.config[n]){
+					t1.config[i]=1;
 				}
 				else{
-					t1[i]=0;
+					t1.config[i]=0;
 				}
 			}
 			return t1;
 		}
 
 		void setDamage(int m){
-			this->dFreq=new int[nCells];
-			this->tDam=new int[nCells];
-			for(int i=0; i<this->nCells; i++){
+			this->dFreq=new int[t0.length];
+			this->tDam=Configuration(t0.length);
+			for(int i=0; i<t0.length; i++){
 				this->dFreq[i]=0;
-				t0[i]=initConfig[i];
+				t0.config[i]=initConfig.config[i];
 				if(i==m){
-					this->tDam[i]=(!this->t0[i]);
+					this->tDam.config[i]=(!this->t0.config[i]);
 				}
 				else{
-					this->tDam[i]=this->t0[i];
+					this->tDam.config[i]=this->t0.config[i];
 				}
 			}
 		}
 
 		void getSpaceEntropy(int size){
-			int* str=new int[size];
+			Configuration str(size);
 			int pSize=pow(2, size), n, i, j, k;
 			this->ps=new double[pSize];
 			this->h=0.0;
 
-			for(i=0; i<(this->nCells)-size; i++){
+			for(i=0; i<(t0.length)-size; i++){
 				k=i;
 				for(j=0; j<size; j++){
-					str[j]=this->t0[k];
+					str.config[j]=this->t0.config[k];
 					k++;
 				}
-				n=binToInt(str, size);
+				n=str.binToInt();
 				this->ps[n]+=1.0;
 			}
 
@@ -192,19 +127,19 @@ class ECA{
 			this->hm=0.0;
 			double p;
 			for(i=0; i<pSize; i++){
-				p=this->ps[i]/static_cast<double>(nCells);
+				p=this->ps[i]/static_cast<double>(t0.length);
 				this->hm+=(p)*(log2(p));
 			}
 			this->hm*=(1.0/static_cast<double>(size))*(-1.0);
 		}
 
 		void phenotipicAnalysis(){
-			int m=this->nCells/2;
+			int m=this->t0.length/2;
 			setDamage(m);
 		}
 		
 		/*void printGen(){
-			for(int i=0; i<(this->nCells); i++){
+			for(int i=0; i<(this->t0.length); i++){
 				if(this->t0[i]){
 					cout << "1" << ends;
 				}
@@ -216,7 +151,7 @@ class ECA{
 		}*/
 
 		/*void printDamageFreq(){
-			for (int i=0; i<this->nCells; i++){
+			for (int i=0; i<this->t0.length; i++){
 				cout << this->dFreq[i] << ends;
 			}
 			cout << "" << endl;
