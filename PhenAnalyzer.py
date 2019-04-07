3666a5323a6af07fe65e9e9a564fefc62512edad
Fabi <fabperaltar@gmail.com>
2019-04-06 22:19:46 -0600

    Entropy function

--- PhenAnalyzer.py
+++ PhenAnalyzer.py
@@ -32,7 +32,7 @@ class PhenAnalyzer:
 			Evolution simulation with a defect introduced.
 	"""
 	dfctPos=0
-	strLength=3
+	strLength=10
 	lyapExp=np.zeros(8, dtype=np.float32)
 	dens=np.zeros(8, dtype=np.uint32)
 	dmgRad=np.zeros(2, dtype=np.uint32)
@@ -87,31 +87,56 @@ class PhenAnalyzer:
 			if(self.lyapExp[i] > 0):
 				self.lyapExp[i]=(1 / t) * (math.log(self.lyapExp[i]))
 
-	def getTopEntropy(self):
-		str=BitString(self.entStringLen)
-		entStringLen=self.entStringLen
-		strProb=np.zeros(2 ** entStringLen, dtype=float)
-		theta=0.0
-		for i in range(self.currentConf.length - entStringLen):
-			k=i
-			for j in range(self.entStringLen):
-				str.bits[j]=self.currentConf.bits[k]
-				k += 1
-			n=str.binToInt()
-			strProb[n] += 1.0
+	def getStrProb(self):
+		numOfStr=2 ** self.strLength
+		totalStr=self.sim.tn.length - self.strLength
+		self.strProb=np.zeros(numOfStr, dtype=float)
+		bs=BitString(self.strLength)
+		for i in range(numOfStr):
+			bs.bsFromInt(i)
+			bs=self.sim.eca.evolve(bs)
+			nextState=bs.binToInt()
+			#print(str(i)+"-->"+str(nextState))
+			self.strProb[nextState] += 1.0
 		
-		for i in range(2 ** entStringLen):
-			if (strProb[i]):
-				strProb[i]=strProb[i] / (2 ** entStringLen)
-				theta += 1
+		for i in range(numOfStr):
+			self.strProb[i] /= numOfStr
+
+		#print(self.strProb)
+
+	def getEntropy(self, totalStr):
+		string=BitString(self.strLength)
+		theta=0.0
+		entropy=0.0
+		for i in range(totalStr):
+			k=i
+			for j in range(self.strLength):
+				string.bits[j]=self.sim.tn.bits[k]
+				k += 1
+			n=string.binToInt()
+			self.strProb[n] += 1.0
+			#print(str(string.bits)+"="+str(n)+" prob="+str(self.strProb[n]))
+		
+		for i in range(len(self.strProb)):
+			if(self.strProb[i]):
+				theta += 1.0
 			
-		self.hX=((1.0 / self.entStringLen) * math.log(theta, 2))
+		#print("log2="+str(math.log(theta, 2)))	
+		if(theta):
+			entropy=((1.0 / self.strLength) * math.log(theta, 2))
+		return entropy
 
 	def runAnalysis(self):
+		totalStr=self.sim.tn.length - self.strLength
+		#self.getStrProb()
+		entropy=np.zeros(self.sim.steps, dtype=np.float32)
 		sScreen=SimScreen(self.damSim.tn.length, self.sim.steps)
 
 		for i in range(self.sim.steps):
+			self.strProb=np.zeros(2 ** self.strLength, dtype=float)
 			sScreen.drawConfiguration(y=i, bitStr=self.sim.tn, dmgBitstr=self.damSim.tn)
+			entropy[i]=self.getEntropy(totalStr)
+			#print("Entropy "+str(i)+"="+str(entropy[i]))
 			for j in range(self.sim.tn.length):
 				if(self.sim.tn.bits[j]):
 					self.dens[i] += 1
@@ -135,17 +160,24 @@ class PhenAnalyzer:
 			self.sim.stepForward()
 			self.countDefects()
 
-		print(self.lyapExp)
+		#print(self.lyapExp)
 		self.getLyapunovExp(self.sim.steps)
-		print(self.lyapExp)
+		#print(self.lyapExp)
 		sScreen.saveToPNG(sScreen.screen, "DamageCone.png")
 		sScreen.openImage("DamageCone.png")
 		plt.figure("Density")
 		plt.plot(self.dens, "m,-")
 		plt.savefig("Density.png")
+		plt.clf()
 		sScreen.openImage("Density.png")
 		plt.figure("Lyapunov exponents")
 		plt.plot(self.lyapExp, "m,-")
 		plt.savefig("LyapunovExp.png")
+		plt.clf()
 		sScreen.openImage("LyapunovExp.png")
+		plt.figure("Entropy")
+		plt.plot(entropy, "m,-")
+		plt.savefig("Entropy.png")
+		plt.clf()
+		sScreen.openImage("Entropy.png")
 		#plt.show()
