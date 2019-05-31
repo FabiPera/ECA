776ba2780b7d552cde034cffb0188991c8755939
Fabi <fabperaltar@gmail.com>
2019-05-30 21:48:44 -0500

    Graph csv generate

---
+++ Graph.py
@@ -0,0 +1,26 @@
+import numpy as np, copy, math, matplotlib.pyplot as plt, csv
+from BitString import BitString
+from ECA import ECA
+
+def getStrProb(strl, eca):
+	numOfStr=2 ** strl
+	ad=[]
+	bs=BitString(strl)
+	for i in range(numOfStr):
+		node=[]
+		node.append(str(i))
+		bs.bsFromInt(i)
+		bs=eca.evolve(bs)
+		nextState=bs.binToInt()
+		node.append(str(nextState))
+		ad.append(node)
+
+	with open("graph.csv", "w") as csvfile:
+		filewriter=csv.writer(csvfile, delimiter=",", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+		filewriter.writerow(['From', 'To'])
+		for x in range(numOfStr):
+			filewriter.writerow(ad[x])
+    
+
+eca=ECA(90, 30)
+getStrProb(30, eca)
