42ae466b5de7e4a99d12e9b46962a96ac976b57a
Fabián Peralta <fabperaltar@gmail.com>
2019-09-29 21:11:05 -0500

    Lyapunov Exponents

--- src/Analysis.py
+++ src/Analysis.py
@@ -6,16 +6,15 @@ from Plotter import *
 class Analysis:
 
 	eca = ECA()
-	ttrow = [1]
+	#ttrow = [1]
 	dfctPos = 0
 	strLength = 16
 	analysisOp = [1, 0, 0]
 	dens = np.zeros(8, dtype=np.uint)
 	dmgRad = np.zeros(2, dtype=np.uint)
-	defects = np.zeros(8, dtype=np.uint)
+	defects = np.zeros(8, dtype=np.double)
+	defectsn = np.zeros(8, dtype=np.double)
 	entropy = np.zeros(8, dtype=np.double)
-	lyapExp = np.zeros(8, dtype=np.double)
-	lyapExpNorm = np.zeros(8, dtype=np.double)
 
 
 	def __init__(self, dfctPos=0, strLength=16, eca=ECA(), analysisOp=[1, 0, 0]):
@@ -23,9 +22,8 @@ class Analysis:
 		self.strLength = strLength
 		self.eca = copy.deepcopy(eca)
 		self.analysisOp = copy.deepcopy(analysisOp)
-		self.defects = np.zeros(eca.x.length, dtype=np.uint)
-		self.lyapExp = np.zeros(eca.x.length, dtype=np.double)
-		self.lyapExpNorm = np.zeros(eca.x.length, dtype=np.double)
+		self.defects = np.zeros(eca.x.length, dtype=np.double)
+		self.defectsn = np.zeros(eca.x.length, dtype=np.double)
 
 	def simAnalysis(self, sim1=Simulation(), sim2=Simulation()):
 		threads = []
@@ -42,34 +40,38 @@ class Analysis:
 				x = threading.Thread(target=self.getEntropy, args=(i, totalStr, sim1.xn))
 				threads.append(x)
 				x.start()
-			if(self.analysisOp[2]):
-				x = threading.Thread(target=self.getTrinomialRow, args=(self.ttrow, ))
-				threads.append(x)
-				x.start()
-				x = threading.Thread(target=self.getConeRadius, args=(i, sim1.xn, sim2.xn))
-				threads.append(x)
-				x.start()
+			# if(self.analysisOp[2]):
+				# x = threading.Thread(target=self.getTrinomialRow, args=(self.ttrow, ))
+				# threads.append(x)
+				# x.start()
+				# x = threading.Thread(target=self.getDefectSpreading, args=(i, sim1.xn, sim2.xn))
+				# threads.append(x)
+				# x.start()
 
 			for x in threads:
 				x.join()
 			
-			self.countDefects(sim1.xn, sim2.xn)
 			sim2.stepForward(i, sim1.xn)
 			sim1.stepForward(i)
+			if(self.analysisOp[2]):
+				self.getDefectSpreading(i, sim1.xn, sim2.xn)
 		
 		sim1.saveToPNG(fileName="SimAnalysis.png")
 		sim2.saveToPNG(fileName="SimDefects.png")
-		threads = []
-		if(self.analysisOp[2]):
-			x = threading.Thread(target=self.getLyapExp, args=(self.ttrow, len(self.defects), sim1.steps))
-			threads.append(x)
-			x.start()
-			x = threading.Thread(target=self.getLyapExp, args=(self.defects, len(self.defects), sim1.steps))
-			threads.append(x)
-			x.start()
 
-		for x in threads:
-			x.join()
+		self.getLyapExp(sim1.steps)
+		
+		# threads = []
+		# if(self.analysisOp[2]):
+		# 	x = threading.Thread(target=self.getLyapExp, args=(self.defectsn, len(self.defectsn), sim1.steps))
+		# 	threads.append(x)
+		# 	x.start()
+		# 	x = threading.Thread(target=self.getLyapExp, args=(self.defects, len(self.defects), sim1.steps))
+		# 	threads.append(x)
+		# 	x.start()
+
+		# for x in threads:
+		# 	x.join()
 
 		if(self.analysisOp[0]):
 			plotDensity(self.dens, sim1.xn.length)
@@ -77,11 +79,11 @@ class Analysis:
 			plotEntropy(self.entropy)
 		if(self.analysisOp[2]):
 			plt.figure("Lyapunov exponents")
-			plt.plot(self.lyapExp, "m,-")
+			plt.plot(self.defects, "m,-")
 			plt.savefig("../sim/LyapunovExp.png")
 			plt.clf()
 			plt.figure("Lyapunov exponents Norm")
-			plt.plot(self.lyapExpNorm, "m,-")
+			plt.plot(self.defectsn, "m,-")
 			plt.savefig("../sim/LyapunovExpNorm.png")
 			plt.clf()
 
@@ -94,10 +96,10 @@ class Analysis:
 		
 		return x
 
-	def getConeRadius(self, y, t, tp):
+	def getDefectSpreading(self, step, t, tp):
 		self.dmgRad[0] = self.dfctPos
 		self.dmgRad[1] = self.dfctPos
-		if(y > 0):
+		if(step > 0):
 			i = 0
 			while(i < self.dfctPos):
 				if(t.bits[i] ^ tp.bits[i]):
@@ -112,8 +114,10 @@ class Analysis:
 					break
 				else:
 					i -= 1
-			if((self.dmgRad[1] - self.dmgRad[0]) > (2 * y)):
+			if((self.dmgRad[1] - self.dmgRad[0]) > (2 * step)):
 				self.dmgRad[0] = self.dfctPos
+
+		self.countDefects(t, tp)
 
 	def getDensity(self, step, xn):
 		n = 0
@@ -124,16 +128,8 @@ class Analysis:
 		n *= 100
 		n = int(n // xn.length)
 
-		#return n
 		self.dens[step] = n
 
-	def countDefects(self, t, tp):
-		if(self.dmgRad[0] == self.dmgRad[1]):
-			self.defects[self.dfctPos] += 1.0
-		else:
-			for x in range(self.dmgRad[0], int(self.dmgRad[1] + 1)):
-				if(t.bits[x] ^ tp.bits[x]):
-					self.defects[x] += 1.0
 
 	def getEntropy(self, step, totalStr, xn):
 		strProb = np.zeros((2 ** self.strLength), dtype=np.double)
@@ -154,45 +150,39 @@ class Analysis:
 			
 		if(theta):
 			entropy=((1.0 / self.strLength) * math.log(theta, 2))
-		
-		#return entropy
+
 		self.entropy[step] = entropy
+	
+	def countDefects(self, t, tp):
+		for c in range(self.dmgRad[0], int(self.dmgRad[1] + 1)):
+			self.defectsn[c] += 1
+			if(t.bits[c] ^ tp.bits[c]):
+				self.defects[c] += 1
 
-	def getTrinomialRow(self, n):
-		if(len(n) == 1):
-			n.append(1)
+	# def getTrinomialRow(self, n):
+	# 	if(len(n) == 1):
+	# 		n.append(1)
 			
-			return n
-		else:
-			nt = []
-			nt.append((n[0] +  (n[1] * 2)))
-			for i in range(1, (len(n))):
-				if(i != (len(n) - 1)):
-					k = n[i - 1] + n[i] + n[i + 1]
-				else:
-					k = n[i - 1] + n[i]	
-				nt.append(k)
-			nt.append(1)
+	# 		return n
+	# 	else:
+	# 		nt = []
+	# 		nt.append((n[0] +  (n[1] * 2)))
+	# 		for i in range(1, (len(n))):
+	# 			if(i != (len(n) - 1)):
+	# 				k = n[i - 1] + n[i] + n[i + 1]
+	# 			else:
+	# 				k = n[i - 1] + n[i]	
+	# 			nt.append(k)
+	# 		nt.append(1)
 
-			#return nt
-			self.ttrow = copy.deepcopy(nt)
+	# 		#return nt
+	# 		self.ttrow = copy.deepcopy(nt)
 
-	def getLyapExp(self, epsilon, n, t):
-		if(len(epsilon) < n):
-			i = len(epsilon) - 1
-			j = 0
-			while(i):
-				print("j=" + str(j) + "i=" + str(i))
-				self.lyapExpNorm[j] = (1 / t) * (math.log(epsilon[i]))
-				j += 1
-				i -= 1
-			print("------------")
-			while(i < len(epsilon) - 1):
-				print("j=" + str(j) + "i=" + str(i))
-				self.lyapExpNorm[j] = (1 / t) * (math.log(epsilon[i]))
-				j += 1
-				i += 1
-		else:
-			for i in range(n):
-				if(epsilon[i] > 0):
-					self.lyapExp[i] = (1 / t) * (math.log(epsilon[i]))
+	def getLyapExp(self, t):
+		for i in range(len(self.defects)):
+			if(self.defects[i] > 0):
+				self.defects[i] = (1 / t) * (math.log(self.defects[i]))
+
+		for i in range(len(self.defectsn)):
+			if(self.defectsn[i] > 0):
+				self.defectsn[i] = (1 / t) * (math.log(self.defectsn[i]))
