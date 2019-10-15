fab76a6fe07af13f16390d670109db95cc63f95d
Fabi <fabperaltar@gmail.com>
2019-10-15 18:53:25 -0500

    Simulation comparison

--- src/Analysis.py
+++ src/Analysis.py
@@ -28,7 +28,8 @@ class Analysis:
 
 	def simAnalysis(self, sim1, sim2, path):
 		threads = []
-		simComparison = Simulation(sim2.steps, sim2.cellSize, sim2.s0Color, sim2.s1Color, sim2.bColor, sim2.dColor, sim2.eca)
+		simComparison1 = Simulation(sim1.steps, sim1.cellSize, sim1.s0Color, sim1.s1Color, sim1.bColor, sim1.dColor, sim1.eca)
+		simComparison2 = Simulation(sim2.steps, sim2.cellSize, sim2.s0Color, sim2.s1Color, sim2.bColor, sim2.dColor, sim2.eca)
 		totalStr = sim1.xn.length - self.strLength
 		self.dens = np.zeros(sim1.steps, dtype=np.uint)
 		self.entropy = np.zeros(sim1.steps, dtype=np.double)
@@ -54,9 +55,12 @@ class Analysis:
 
 			for x in threads:
 				x.join()
-			
-			simComparison.draw(i, int(self.dmgRad[0]), int(self.dmgRad[1] + 1), simComparison.xn)
-			simComparison.xn = copy.deepcopy(simComparison.eca.evolve(simComparison.xn))
+
+			simComparison1.draw(i, int(self.dmgRad[0]), int(self.dmgRad[1] + 1), simComparison1.xn)
+			simComparison1.xn = copy.deepcopy(simComparison1.eca.evolve(simComparison1.xn))
+			simComparison2.draw(i, int(self.dmgRad[0]), int(self.dmgRad[1] + 1), simComparison2.xn)
+			simComparison2.xn = copy.deepcopy(simComparison2.eca.evolve(simComparison2.xn))
+
 			sim2.stepForward(i, sim1.xn)
 			sim1.stepForward(i)
 
@@ -67,7 +71,8 @@ class Analysis:
 		
 		sim1.saveToPNG(path, "SimAnalysis.png")
 		sim2.saveToPNG(path, "SimDefects.png")
-		simComparison.saveToPNG(path, "SimComparison.png")
+		simComparison1.saveToPNG(path, "SimOriginal.png")
+		simComparison2.saveToPNG(path, "SimAlter.png")
 
 		self.getLyapExp(sim1.steps)
 		print(self.defects[self.dfctPos])
